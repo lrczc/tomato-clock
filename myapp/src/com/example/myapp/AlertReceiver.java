b76951880d4577af3f15f687e512035e199af008
shizhao.czc <shizhao.czc@alibaba-inc.com>
2014-09-01 17:13:31 +0800

    初始化每日提醒service

---
+++ myapp/src/com/example/myapp/AlertReceiver.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by shizhao.czc on 2014/9/1.
+ */
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
